dd1fddf096965f32c111433b6c7100b88cde2f47
Sunofokkusu <justine.pruliere4@etu.univ-lorraine.fr>
2023-04-24 16:01:46 +0200

    fix

--- main.js
+++ main.js
@@ -6,11 +6,11 @@ import { PointerLockControls } from "three/addons/controls/PointerLockControls.j
 
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 
-import { VRButton } from 'three/examples/jsm/webxr/VRButton.js';
+import { VRButton } from "three/examples/jsm/webxr/VRButton.js";
 
-import { XRControllerModelFactory } from 'three/examples/jsm/webxr/XRControllerModelFactory.js';
+import { XRControllerModelFactory } from "three/examples/jsm/webxr/XRControllerModelFactory.js";
 
 let moveForward = false;
 let moveBackward = false;
@@ -20,12 +20,17 @@ let moveRight = false;
 const scene = new THREE.Scene();
 
 // add a ground picture
-const groundTexture = new THREE.TextureLoader().load("./depositphotos_10589691-stock-photo-ground-background.webp");
+const groundTexture = new THREE.TextureLoader().load(
+  "./depositphotos_10589691-stock-photo-ground-background.webp"
+);
 groundTexture.wrapS = THREE.RepeatWrapping;
 groundTexture.wrapT = THREE.RepeatWrapping;
 groundTexture.repeat.set(100, 100);
 const groundMaterial = new THREE.MeshLambertMaterial({ map: groundTexture });
-const groundMesh = new THREE.Mesh(new THREE.PlaneGeometry(10000, 10000), groundMaterial);
+const groundMesh = new THREE.Mesh(
+  new THREE.PlaneGeometry(10000, 10000),
+  groundMaterial
+);
 groundMesh.rotation.x = -Math.PI / 2;
 groundMesh.position.y = -5;
 scene.add(groundMesh);
@@ -50,20 +55,19 @@ scene.add(pointLight);
 
 // reduce the amount of light in the scene
 
-
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(ambientLight);
 
-function addTree(number){
-    for(let i = 0; i < number; i++){
-      let loader = new FBXLoader();
-      loader.load('./oak_01.fbx', function (object) {
-            let randX = Math.random() * 200 - 100;
-            let randZ = Math.random() * 200 - 100;
-            object.position.set(randX, -5, randZ);
-            scene.add(object);
-        });
-    }
+function addTree(number) {
+  for (let i = 0; i < number; i++) {
+    let loader = new FBXLoader();
+    loader.load("./oak_01.fbx", function (object) {
+      let randX = Math.random() * 200 - 100;
+      let randZ = Math.random() * 200 - 100;
+      object.position.set(randX, -5, randZ);
+      scene.add(object);
+    });
+  }
 }
 
 addTree(5);
@@ -82,10 +86,10 @@ window.addEventListener("resize", () => {
   camera.updateProjectionMatrix();
 });
 
-document.body.appendChild( VRButton.createButton( renderer ) );
+document.body.appendChild(VRButton.createButton(renderer));
 renderer.xr.enabled = true;
 renderer.setAnimationLoop(function () {
-    renderer.render( scene, camera );
+  renderer.render(scene, camera);
 });
 
 let gripController1 = renderer.xr.getControllerGrip(0);
@@ -100,13 +104,68 @@ gripController2.add(controllerModel2);
 scene.add(gripController1);
 scene.add(gripController2);
 
-const lineSegments=10;
+const lineSegments = 10;
 const lineGeometry = new THREE.BufferGeometry();
-const lineGeometryVertices = new Float32Array((lineSegments +1) * 3);
+const lineGeometryVertices = new Float32Array((lineSegments + 1) * 3);
 lineGeometryVertices.fill(0);
-lineGeometry.setAttribute('position', new THREE.BufferAttribute(lineGeometryVertices, 3));
-const lineMaterial = new THREE.LineBasicMaterial({ color: 0x888888, blending: AdditiveBlending });
-const guideline = new THREE.Line( lineGeometry, lineMaterial );
+lineGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(lineGeometryVertices, 3)
+);
+const lineMaterial = new THREE.LineBasicMaterial({
+  color: 0x888888,
+  blending: AdditiveBlending,
+});
+const guideline = new THREE.Line(lineGeometry, lineMaterial);
+const guideLight = new THREE.PointLight(0xffeeaa, 0, 2);
+
+function positionAtT(inVec, t, p, v, g) {
+  inVec.copy(p);
+  inVec.addScaledVector(v, t);
+  inVec.addScaledVector(g, 0.5 * t ** 2);
+  return inVec;
+}
+
+let guidingController = null;
+function onSelectStart(e) {
+  if (e && e.data && e.data.hand) {
+    return;
+  }
+
+  guidingController = this;
+  guideLight.intensity = 1;
+  guidingController.add(guideline);
+  scene.add(guideSprite);
+}
+
+function onSelectEnd() {
+  if (guidingController === this) {
+    // first work out vector from feet to cursor
+
+    // feet position
+    const feetPos = renderer.xr.getCamera(camera).getWorldPosition(tempVec);
+    feetPos.y = 0;
+
+    // cursor position
+    const p = guidingController.getWorldPosition(tempVecP);
+    const v = guidingController.getWorldDirection(tempVecV);
+    v.multiplyScalar(6);
+    const t = (-v.y + Math.sqrt(v.y ** 2 - 2 * p.y * g.y)) / g.y;
+    const cursorPos = positionAtT(tempVec1, t, p, v, g);
+
+    // Offset
+    const offset = cursorPos.addScaledVector(feetPos, -1);
+
+    // Do the locomotion
+    locomotion(offset);
+
+    // clean up
+    guidingController = null;
+    guideLight.intensity = 0;
+    this.remove(guideline);
+    scene.remove(guideSprite);
+  }
+}
 
 gripController1.addEventListener("selectstart", onSelectStart);
 gripController1.addEventListener("selectend", onSelectEnd);
@@ -115,14 +174,11 @@ gripController2.addEventListener("selectstart", onSelectStart);
 gripController2.addEventListener("selectend", onSelectEnd);
 
 const loader = new GLTFLoader();
-loader.load(
-  "voiture.glb",
-  (gltf) => {
-    gltf.scene.position.setY(-5);
-    gltf.scene.position.setX(12);
-    scene.add(gltf.scene);
-  }
-);
+loader.load("voiture.glb", (gltf) => {
+  gltf.scene.position.setY(-5);
+  gltf.scene.position.setX(12);
+  scene.add(gltf.scene);
+});
 let controls = new PointerLockControls(camera, renderer.domElement);
 
 controls.addEventListener("lock", (event) => {

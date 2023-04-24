742e1a8f8d2433c52b4ea402a85c532fd7abce74
Sunofokkusu <justine.pruliere4@etu.univ-lorraine.fr>
2023-04-24 14:05:01 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,262 @@
+import "./style.css";
+
+import * as THREE from "three";
+
+import { PointerLockControls } from "three/addons/controls/PointerLockControls.js";
+
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
+
+import { VRButton } from 'three/examples/jsm/webxr/VRButton.js';
+
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
+
+const scene = new THREE.Scene();
+
+// add a ground picture
+const groundTexture = new THREE.TextureLoader().load("./depositphotos_10589691-stock-photo-ground-background.webp");
+groundTexture.wrapS = THREE.RepeatWrapping;
+groundTexture.wrapT = THREE.RepeatWrapping;
+groundTexture.repeat.set(100, 100);
+const groundMaterial = new THREE.MeshLambertMaterial({ map: groundTexture });
+const groundMesh = new THREE.Mesh(new THREE.PlaneGeometry(10000, 10000), groundMaterial);
+groundMesh.rotation.x = -Math.PI / 2;
+groundMesh.position.y = -0.1;
+scene.add(groundMesh);
+
+// add a sky picture
+const skyTexture = new THREE.TextureLoader().load("./istockphoto-825778252-612x612.jpg");
+const skyMaterial = new THREE.MeshLambertMaterial({ map: skyTexture });
+const skyMesh = new THREE.Mesh(new THREE.PlaneGeometry(10000, 10000), skyMaterial);
+skyMesh.rotation.x = -Math.PI / 2;
+skyMesh.position.y = 10;
+scene.add(skyMesh);
+
+let raycaster;
+const objects = [];
+ 
+let threeHitbox = [];
+
+let prevTime = performance.now();
+const velocity = new THREE.Vector3();
+const direction = new THREE.Vector3();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.setY(12);
+camera.position.setZ(-4);
+camera.rotateY(3.15);
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+
+scene.add(pointLight);
+
+// reduce the amount of light in the scene
+
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(ambientLight);
+
+function addTree(number){
+    for(let i = 0; i < number; i++){
+      let loader = new FBXLoader();
+      loader.load('./oak_01.fbx', function (object) {
+            let randX = Math.random() * 200 - 100;
+            let randZ = Math.random() * 200 - 100;
+            object.position.set(randX, 0, randZ);
+            let hitbox = new THREE.Box3().setFromObject(object);
+            hitbox.min.x += 15;
+            hitbox.min.z += 15;
+            hitbox.max.x -= 15;
+            hitbox.max.z -= 15;
+            // create four hitboxes for each tree
+            let hitbox1 = new THREE.Box3(new THREE.Vector3(hitbox.min.x, hitbox.min.y, hitbox.min.z), new THREE.Vector3(hitbox.min.x, hitbox.max.y, hitbox.max.z));
+            let hitbox2 = new THREE.Box3(new THREE.Vector3(hitbox.min.x, hitbox.min.y, hitbox.min.z), new THREE.Vector3(hitbox.max.x, hitbox.max.y, hitbox.min.z));
+            let hitbox3 = new THREE.Box3(new THREE.Vector3(hitbox.max.x, hitbox.min.y, hitbox.min.z), new THREE.Vector3(hitbox.max.x, hitbox.max.y, hitbox.max.z));
+            let hitbox4 = new THREE.Box3(new THREE.Vector3(hitbox.min.x, hitbox.min.y, hitbox.max.z), new THREE.Vector3(hitbox.max.x, hitbox.max.y, hitbox.max.z));
+
+            // create a helper for each hitbox
+            let helper1 = new THREE.Box3Helper(hitbox1, 0xffff00);
+            let helper2 = new THREE.Box3Helper(hitbox2, 0xffff00);
+            let helper3 = new THREE.Box3Helper(hitbox3, 0xffff00);
+            let helper4 = new THREE.Box3Helper(hitbox4, 0xffff00);
+
+            threeHitbox.push([hitbox1, hitbox2, hitbox3, hitbox4]);
+
+            scene.add(helper1);
+            scene.add(helper2);
+            scene.add(helper3);
+            scene.add(helper4);
+            scene.add(object);
+        });
+    }
+}
+
+addTree(5);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#bg"),
+  antialias: false,
+});
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio * 0.5);
+
+window.addEventListener("resize", () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+});
+
+document.body.appendChild( VRButton.createButton( renderer ) );
+renderer.xr.enabled = true;
+renderer.setAnimationLoop(function () {
+    renderer.render( scene, camera );
+});
+
+
+const loader = new GLTFLoader();
+loader.load(
+  "voiture.glb",
+  (gltf) => {
+    gltf.scene.position.setY(5);
+    scene.add(gltf.scene);
+  }
+);
+let controls = new PointerLockControls(camera, renderer.domElement);
+
+controls.addEventListener("lock", (event) => {
+  document.body.style.cursor = "none";
+});
+
+controls.addEventListener("unlock", () => {
+  document.body.style.cursor = "auto";
+});
+
+document.body.addEventListener("click", () => {
+  controls.lock();
+});
+
+scene.add(controls.getObject());
+
+raycaster = new THREE.Raycaster(
+  new THREE.Vector3(),
+  new THREE.Vector3(0, -1, 0),
+  0,
+  10
+);
+
+function animate() {
+  requestAnimationFrame(animate);
+  const time = performance.now();
+  let directionCollision = "none";
+  if (controls.isLocked === true) {
+    raycaster.ray.origin.copy(controls.getObject().position);
+    raycaster.ray.origin.y -= 10;
+
+    const intersections = raycaster.intersectObjects(objects);
+    let onObject = intersections.length > 0;
+    if (carHitbox) {
+      threeHitbox.forEach((hitbox) => {
+        if (carHitbox.intersectsBox(hitbox[0])) {
+          onObject = true;
+          directionCollision = "front";
+        } else if (carHitbox.intersectsBox(hitbox[1])) {
+          onObject = true;
+          directionCollision = "left";
+        } else if (carHitbox.intersectsBox(hitbox[2])) {
+          onObject = true;
+          directionCollision = "right";
+        } else if (carHitbox.intersectsBox(hitbox[3])) {
+          onObject = true;
+          directionCollision = "back";
+        }
+      });
+    }
+
+    const delta = (time - prevTime) / 1000;
+
+    velocity.x -= velocity.x * 10.0 * delta;
+    velocity.z -= velocity.z * 10.0 * delta;
+
+    velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
+
+    direction.z = Number(moveForward) - Number(moveBackward);
+    direction.x = Number(moveRight) - Number(moveLeft);
+    direction.normalize(); // this ensures consistent movements in all directions
+
+    if (moveForward || moveBackward) velocity.z -= direction.z * 400.0 * delta;
+    if (moveLeft || moveRight) velocity.x -= direction.x * 400.0 * delta;
+
+    if (onObject === true) {
+      velocity.y = Math.max(0, velocity.y);
+      if(directionCollision === "front") {
+        camera.position.z -= 0.1;
+      }
+      if(directionCollision === "back") {
+        camera.position.z += 0.1;
+      }
+      if(directionCollision === "right") {
+        camera.position.x -= 0.1;
+      }
+      if(directionCollision === "left") {
+        camera.position.x += 0.1;
+      }
+    } else {
+      controls.moveRight(-velocity.x * delta);
+      controls.moveForward(-velocity.z * delta);
+
+      controls.getObject().position.y += velocity.y * delta; // new behavior
+
+      if (controls.getObject().position.y < 10) {
+        velocity.y = 0;
+        controls.getObject().position.y = 10;
+      }
+    }
+  }
+
+  prevTime = time;
+
+  renderer.render(scene, camera);
+}
+
+animate();
+
+document.addEventListener("keydown", function (event) {
+  if (event.code === "ArrowUp" || event.code === "KeyW") {
+    moveForward = true;
+  }
+  if (event.code === "ArrowDown" || event.code === "KeyS") {
+    moveBackward = true;
+  }
+  if (event.code === "ArrowLeft" || event.code === "KeyA") {
+    moveLeft = true;
+  }
+  if (event.code === "ArrowRight" || event.code === "KeyD") {
+    moveRight = true;
+  }
+});
+
+document.addEventListener("keyup", function (event) {
+  if (event.code === "ArrowUp" || event.code === "KeyW") {
+    moveForward = false;
+  }
+  if (event.code === "ArrowDown" || event.code === "KeyS") {
+    moveBackward = false;
+  }
+  if (event.code === "ArrowLeft" || event.code === "KeyA") {
+    moveLeft = false;
+  }
+  if (event.code === "ArrowRight" || event.code === "KeyD") {
+    moveRight = false;
+  }
+});
